123d84d1d9439dae0855b68ebdb682167d9d745d
Dhruv020405 <dhruvpandya0204@gmail.com>
2026-03-26 22:56:56 +0530

    Initial commit

---
+++ onlinecourse/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+
+class Question(models.Model):
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+class Enrollment(models.Model):
+    user = models.CharField(max_length=100)
+
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
