1e85452543d53f7752d2521a78038f335d1b2591
Dhruv020405 <dhruvpandya0204@gmail.com>
2026-03-26 23:13:46 +0530

    Update views.py

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Submission, Choice
 
-# Create your views here.
+def submit(request, course_id):
+    if request.method == 'POST':
+        selected_choices = request.POST.getlist('choice')
+        
+        submission = Submission.objects.create(enrollment_id=1)
+        submission.choices.set(selected_choices)
+
+        return redirect('show_exam_result', submission_id=submission.id)
+
+def show_exam_result(request, submission_id):
+    submission = Submission.objects.get(id=submission_id)
+    
+    selected_choices = submission.choices.all()
+    score = selected_choices.count()
+
+    context = {
+        'score': score,
+        'choices': selected_choices
+    }
+
+    return render(request, 'result.html', context)
